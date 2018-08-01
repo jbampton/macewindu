5c85f7a92863e2433269df7d46c3db7198e0d14d
John Bampton <jbampton@users.noreply.github.com>
2018-08-02 04:38:51 +1000

    Scrape some Google data

--- main.go
+++ main.go
@@ -6,6 +6,10 @@ import (
 	"os"
 	"github.com/gin-gonic/gin"
 	_ "github.com/heroku/x/hmetrics/onload"
+	"html/template"
+	"github.com/gocolly/colly"
+	"time"
+	"fmt"
 )
 
 func main() {
@@ -18,7 +22,41 @@ func main() {
 	router.LoadHTMLGlob("templates/*.tmpl.html")
 	router.Static("/static", "static")
 	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl.html", nil)
+		c.HTML(http.StatusOK, "index.tmpl.html", map[string]interface{}{
+			"results": template.HTML(scrapeGoogle()),
+		})
 	})
 	router.Run(":" + port)
+}
+
+func scrapeGoogle() string {
+	startTime := gettime(time.Now().Unix())
+	result := ""
+	google := "www.google.com"
+	co := colly.NewCollector(
+		colly.AllowedDomains(google),
+		//colly.CacheDir("./macewindu_cache"),
+	)
+	co.OnRequest(func(r *colly.Request) {
+		result += startTime + fmt.Sprintf("Visiting: <a href=\"%s\" target=\"_blank\" rel=\"noreferrer\">%s</a><br><br>\n", r.URL, r.URL)
+	})
+
+	// Find all links
+	co.OnHTML("a[href^='/url?q']", func(e *colly.HTMLElement) {
+		if len(e.Text) > 0 && e.Text != "Cached"  { // fix ??
+			result += "<a href=\"" + "https://" + google + e.Attr("href") +
+				"\" target=\"_blank\" rel=\"noreferrer\">" + e.Text + "</a><br>\n"
+		}
+	})
+
+	co.OnScraped(func(r *colly.Response) {
+		result += fmt.Sprintf("Finished: %s", gettime(time.Now().Unix()))
+	})
+
+	co.Visit("https://www.google.com/search?q=mace+windu")
+	return result
+}
+
+func gettime(i int64) string {
+	return fmt.Sprintf("Current Unix Time: %v<br>", i)
 }
